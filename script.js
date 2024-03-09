53b47de061c6a7475ae6162da8131889acb80102
flushthemoney <skaviraj562@gmail.com>
2024-03-09 21:24:37 +0530

    Basic Shopping List [Completed]
    Signed-off-by: flushthemoney <skaviraj562@gmail.com>

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const list = document.querySelector("#shoppingList");
+const inputBox = document.querySelector("#input-box");
 const addButton = document.querySelector("#addbtn");
 let removeButtons = document.querySelectorAll("#shoppingList button");
 
-addButton.addEventListener("click", () => {
-  addItem(document.querySelector("#input-box").value);
+function addButtonFunction() {
+  addItem(inputBox.value);
   removeButtons = document.querySelectorAll("#shoppingList button");
+  removeButtonListener();
+  inputBox.value = "";
+  inputBox.focus();
+}
+
+addButton.addEventListener("click", addButtonFunction);
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addButtonFunction();
+  }
 });
 
-removeButtons.forEach((removeButton) => {
-  removeButton.addEventListener("click", alert("deleted"));
-});
+function removeItem() {
+  this.parentNode.remove();
+}
+
+function removeButtonListener() {
+  for (let button of removeButtons) {
+    button.addEventListener("click", removeItem);
+  }
+}
 
 function addItem(text) {
   const newListItem = document.createElement("div");
